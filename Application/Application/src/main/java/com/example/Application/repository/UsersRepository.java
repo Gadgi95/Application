10569b6e6385b29c6593da7748df649b35ccfcb2
Vadim <h4998998482@gmail.com>
2023-01-11 22:56:48 +0300

    add repository

---
+++ Application/Application/src/main/java/com/example/Application/repository/UsersRepository.java
@@ -0,0 +1,7 @@
+package com.example.Application.repository;
+
+import com.example.Application.model.Users;
+
+public interface UsersRepository {
+    Users save(Users user);
+}
---
+++ Application/Application/src/main/java/com/example/Application/repository/datajpa/DataJpaUsersRepository.java
@@ -0,0 +1,20 @@
+package com.example.Application.repository.datajpa;
+
+import com.example.Application.model.Users;
+import com.example.Application.repository.UsersRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DataJpaUsersRepository implements UsersRepository {
+
+    private final CrudUsersRepository crudRepository;
+
+    public DataJpaUsersRepository(CrudUsersRepository crudRepository) {
+        this.crudRepository = crudRepository;
+    }
+
+    @Override
+    public Users save(Users user) {
+        return crudRepository.save(user);
+    }
+}
