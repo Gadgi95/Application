5d8d68e4feb14b82b74ceedd99dada81661c50d3
Vadim <h4998998482@gmail.com>
2023-01-11 22:15:29 +0300

    add script initDB

---
+++ Application/Application/src/main/resources/db/initDB.sql
@@ -0,0 +1,22 @@
+CREATE TABLE User (
+                      id INT AUTO_INCREMENT PRIMARY KEY,
+                      name VARCHAR(255) NOT NULL,
+                      mail VARCHAR(255) NOT NULL,
+                      ROLE ENUM('admin', 'user') NOT NULL
+);
+
+CREATE TABLE Application (
+                             id INT AUTO_INCREMENT PRIMARY KEY,
+                             name VARCHAR(255) NOT NULL,
+                             creationDate DATE NOT NULL,
+                             status ENUM('NEW', 'IN_PROGRESS', 'CLOSED') NOT NULL
+);
+
+CREATE TABLE archive (
+                         id INT AUTO_INCREMENT PRIMARY KEY,
+                         name VARCHAR(255) NOT NULL,
+                         creationDate DATE NOT NULL,
+                         closingDate DATE NOT NULL,
+                         closedBy INT,
+                         FOREIGN KEY (closedBy) REFERENCES User(id)
+);
