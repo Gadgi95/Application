3ebad3a7da136548a81d456bbbd783be1eb85bb4
Vadim <h4998998482@gmail.com>
2023-01-16 23:31:45 +0300

    adding class ObjectName

--- Application/Application/src/main/resources/db/initDB.sql
+++ Application/Application/src/main/resources/db/initDB.sql
@@ -1,11 +1,11 @@
-CREATE TABLE User (
+CREATE TABLE users (
                       id INT AUTO_INCREMENT PRIMARY KEY,
                       name VARCHAR(255) NOT NULL,
                       mail VARCHAR(255) NOT NULL,
                       ROLE ENUM('admin', 'user') NOT NULL
 );
 
-CREATE TABLE Ticket (
+CREATE TABLE tickets (
                              id INT AUTO_INCREMENT PRIMARY KEY,
                              user_id INT NOT NULL,
                              name VARCHAR(255) NOT NULL,
@@ -13,11 +13,11 @@ CREATE TABLE Ticket (
                              status ENUM('NEW', 'IN_PROGRESS', 'CLOSED') NOT NULL
 );
 
-CREATE TABLE archive (
+CREATE TABLE tickets_archive (
                          id INT AUTO_INCREMENT PRIMARY KEY,
                          name VARCHAR(255) NOT NULL,
                          creationDate DATE NOT NULL,
                          closingDate DATE NOT NULL,
                          closedBy INT,
-                         FOREIGN KEY (closedBy) REFERENCES User(id)
+                         FOREIGN KEY (closedBy) REFERENCES users(id)
 );
